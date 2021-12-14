cc359583a88be15070a569051ee584b3b1d9c8ec
Marcos Carvalho <mvgc1989@gmail.com>
2021-12-14 11:53:35 -0300

    Modal e Forms Final

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,12 @@
 import Modal from 'react-modal'
-import { Container, TransactionTypeContainer } from '../NewTransactionModal/styles'
+import { Container, TransactionTypeContainer, RadioBox} from '../NewTransactionModal/styles'
 
 
 import IncomeImg from '../../assets/income.svg'
 import OutcomeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg'
+import { FormEvent, useState } from 'react'
+import { api } from '../../services/api'
 
 
 interface NewTransactionProps {
@@ -13,6 +15,22 @@ interface NewTransactionProps {
 }
 
 export function NewTransactionModal ({ isOpen, onRequestClose}: NewTransactionProps){
+    const [title, setTitle] = useState('')
+    const [value, setValue] = useState(0)
+    const [category, setCategory] = useState('')
+    
+
+    const [type, setType] = useState('deposit')
+
+    function handleCreateNewTransaction(event: FormEvent){
+        event.preventDefault()
+
+        const data = {title, value, category,type}
+        
+        api.post('/transactions', data)
+    }
+
+
     return (
         <Modal 
             isOpen={isOpen}
@@ -28,34 +46,47 @@ export function NewTransactionModal ({ isOpen, onRequestClose}: NewTransactionPr
                 <img src={closeImg} alt="close" />
             </button>
 
-            <Container> 
+            <Container onSubmit={handleCreateNewTransaction}> 
                 <h2>Cadastrar Transação</h2>
                 
                 <input 
                     placeholder="Título"
+                    value={title}
+                    onChange={event => setTitle(event.target.value)}
                 />
 
                 <input 
                     type="number"
                     placeholder="Valor"
+                    value={value}
+                    onChange={event => setValue(Number(event.target.value))}
                 />
 
                 <TransactionTypeContainer>
-                    <button 
+                    <RadioBox 
+                        onClick={() => {setType ('deposit')}}
+                        isActive={type === 'deposit'}
+                        activeColor="green"
                         type="button">
                         <img src={IncomeImg} alt="entrada" />
                         <span>Entrada</span>
-                    </button>
+                    </RadioBox>
 
-                    <button 
-                        type="button">
+                    <RadioBox 
+                        type="button"
+                        isActive={type === 'withdraw'}
+                        activeColor="red"
+                        onClick={() => {setType ('withdraw')}}>
+                        
                         <img src={OutcomeImg} alt="saida" />
                         <span>Saída</span>
-                    </button>
+                    </RadioBox>
                 </TransactionTypeContainer>
                 
                 <input 
                     placeholder="Categoria"
+                    value={category}
+                    onChange={event => setCategory(event.target.value)}
                 />
 
                 <button type="submit">Cadastrar</button>
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import {App} from './App';
 
+
 createServer({
+  models:{
+    transaction: Model
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Frelancer de website",
+          type: 'deposit',
+          category:'Trabalho',
+          amount:6000,
+          createdAt: new Date ('2021-12-13 18:00:00'),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: 'withdraw',
+          category:'Casa',
+          amount:2200,
+          createdAt: new Date ('2021-12-06 10:00:00'),
+        },
+      ],
+    })
+  },
+
   routes(){
     this.namespace = 'api'
 
     this.get('/transactions', ()=>{
-      return [{
-        id:1,
-        title:'trasaction1',
-        amount:300,
-        type:'deposit',
-        category: 'food',
-        createdAt: new Date()
-      }]
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request)=> {
+      const data = JSON.parse(request.requestBody)
+      return schema.create('transaction', data)
     })
   }
 })
